78246afb4d62a45cb298d36f61adf22180136da5
Ashley Wenger <ashley-wenger>
2019-10-12 17:05:30 -0600

    Added logic to get the customer's lat/long and the seller's lat/long

--- creating merged dataset.py
+++ creating merged dataset.py
@@ -78,7 +78,7 @@ sum(pd.isnull(df_sellers.seller_city))   #0, perfect, always populated
 
 df_order_pmts.info()   #103,886
 sum(pd.isnull(df_order_pmts.order_id))   #0, perfect, always populated
-len(pd.unique(df_order_pmts.order_id))   #1 less than # of orders ==> one is duplicated somewhere.  track down and check later
+len(pd.unique(df_order_pmts.order_id))   #1 less than # of orders ==> one has no pmt.  track down and check later
 sum(pd.isnull(df_order_pmts.payment_type))   #0, perfect, always populated
 
 
@@ -154,12 +154,212 @@ df_merged.info()  #now have 118,318 as a few orders have multiple pmt dtls rows 
 sum(pd.isnull(df_merged.payment_type))   #3,   almost perfect,  the join always succeeded except for 3 records
 
 
+#write out to CSV file to share
 df_merged.to_csv(wkg_dir+'Merged_dataset.csv')
 
+
+
+
+
+
+# ==============================
+# add lat/longs for the customer
+# ==============================
 #customer_id	a.customer_unique_id	a.customer_zip_code_prefix	a.customer_city	a.customer_state	b.order_id	b.order_status	b.order_purchase_timestamp	b.order_approved_at	b.order_delivered_carrier_date	b.order_delivered_customer_date	b.order_estimated_delivery_date	c.payment_type	c.payment_installments	c.payment_value
 
 
+#            Lat_Avg=pd.NamedAgg('geolocation_lat', aggfunc='mean'),
+
+#sum(pd.isnull(df_geolocation.geolocation_zip_code_prefix))  #0, perfect always populated
+#sum(pd.isnull(df_geolocation.geolocation_city))  #0, perfect always populated
+#sum(pd.isnull(df_geolocation.geolocation_state))  #0, perfect always populated
+#sum(pd.isnull(df_geolocation.geolocation_lat))  #0, perfect always populated
 #add lat/long info for the customers
-#df_merged = df_merged.merge(df_geolocation, how='left', left_on='customer_zip_code_prefix', right_on='geolocation_zip_code_prefix')
-#df_merged.info()  #now have 118,318 as a few orders have multiple pmt dtls rows (and/or dup order ids?)
-#
+df_geo_avg_by_zip_city_state = df_geolocation.groupby(['geolocation_zip_code_prefix', 'geolocation_city', 'geolocation_state']).agg(
+            Nbr_Rcds=('geolocation_lat', 'count'),
+            Lat_Avg=('geolocation_lat', 'mean'),
+            Lat_Min=('geolocation_lat', 'min'),
+            Lat_Max=('geolocation_lat', 'max'),
+            Long_Avg=('geolocation_lng', 'mean'),
+            Long_Min=('geolocation_lng', 'min'),
+            Long_Max=('geolocation_lng', 'max'))
+
+#not sure why this was necessary (setting as_index=False gets rid of the multi-index, but did not return the groupby columns as col's in the output?!??) 
+# but this turns the MultiIndex for the rows into columns of data as desired.
+df_geo_avg_by_zip_city_state = df_geo_avg_by_zip_city_state.reset_index()
+df_geo_avg_by_zip_city_state.info()   #27,912 rows
+df_geo_avg_by_zip_city_state.head()
+
+df_geo_avg_by_zip_city_state
+#after reviewing, drop extra columns and simplify the names
+df_geo_avg_by_zip_city_state2 = df_geo_avg_by_zip_city_state[['geolocation_zip_code_prefix', 'geolocation_city', 'geolocation_state', 'Lat_Avg', 'Long_Avg']]
+df_geo_avg_by_zip_city_state2.columns = ['zip_prefix', 'city', 'state', 'lat', 'long']
+
+
+
+
+df_geo_avg_by_zip_city = df_geolocation.groupby(['geolocation_zip_code_prefix', 'geolocation_city']).agg(
+            Nbr_Rcds=('geolocation_lat', 'count'),
+            Lat_Avg=('geolocation_lat', 'mean'),
+            Lat_Min=('geolocation_lat', 'min'),
+            Lat_Max=('geolocation_lat', 'max'),
+            Long_Avg=('geolocation_lng', 'mean'),
+            Long_Min=('geolocation_lng', 'min'),
+            Long_Max=('geolocation_lng', 'max'))
+
+df_geo_avg_by_zip_city = df_geo_avg_by_zip_city.reset_index()
+df_geo_avg_by_zip_city.info()   #27,907 rows
+df_geo_avg_by_zip_city.head()
+
+#after reviewing, drop extra columns and simplify the names
+df_geo_avg_by_zip_city2 = df_geo_avg_by_zip_city[['geolocation_zip_code_prefix', 'geolocation_city', 'Lat_Avg', 'Long_Avg']]
+df_geo_avg_by_zip_city2.columns = ['zip_prefix', 'city', 'lat', 'long']
+df_geo_avg_by_zip_city2.info()
+
+
+
+df_geo_avg_by_zip = df_geolocation.groupby(['geolocation_zip_code_prefix']).agg(
+            Nbr_Rcds=('geolocation_lat', 'count'),
+            Lat_Avg=('geolocation_lat', 'mean'),
+            Lat_Min=('geolocation_lat', 'min'),
+            Lat_Max=('geolocation_lat', 'max'),
+            Long_Avg=('geolocation_lng', 'mean'),
+            Long_Min=('geolocation_lng', 'min'),
+            Long_Max=('geolocation_lng', 'max'))
+
+df_geo_avg_by_zip = df_geo_avg_by_zip.reset_index()
+df_geo_avg_by_zip.info()
+
+df_geo_avg_by_zip2 = df_geo_avg_by_zip[['geolocation_zip_code_prefix', 'Lat_Avg', 'Long_Avg']]
+df_geo_avg_by_zip2.columns = ['zip_prefix', 'lat', 'long']
+df_geo_avg_by_zip2.info()
+
+
+
+df_merged.info()
+
+df_merged2 = df_merged.merge(df_geo_avg_by_zip_city_state2, how='left', left_on=['customer_zip_code_prefix', 'customer_city', 'customer_state'], right_on=['zip_prefix', 'city', 'state'])
+#df_merged2.info()  #stiil have 118,318 as expected
+sum(pd.isnull(df_merged2.lat))  #369     
+sum(pd.isna(df_merged2.customer_zip_code_prefix))  #0     
+sum(pd.isnull(df_merged.customer_city))  #0     
+sum(pd.isnull(df_merged.customer_state))  #0 
+    #apparently some of the custs are from zip/city/states not in the lat/long lookup table.   retry by zip and state
+    
+#prune out these join columns that have been added in unnecessarily
+df_merged2.drop(['zip_prefix', 'city', 'state'], axis='columns', inplace=True)
+#add a row ID so we can easily join in missing results if we find them in the subsequent steps
+df_merged2.insert(0, 'Rww_ID', range(0, len(df_merged2)))
+
+
+
+
+#try joining on just zip & city and see if that succeeds (maybe the states were null or conflicting??)
+   
+#for the 2nd attempt at joining on zip&city, prune out the lat & long columns as a) they are null anyway and b) they cause diff lat&long cols to get created like lat_y, long_y
+cols = [col for col in df_merged2.columns if col not in ['lat', 'long']]
+df_merge_by_zip_and_city = df_merged2[ pd.isnull(df_merged2.lat) ][ cols ].merge(df_geo_avg_by_zip_city2, how='left', left_on=['customer_zip_code_prefix', 'customer_city'], right_on=['zip_prefix', 'city'])
+df_merge_by_zip_and_city.drop(['zip_prefix', 'city'], axis='columns', inplace=True)
+df_merge_by_zip_and_city.info()    #369 rows attempted but still no success in joining
+sum(pd.isnull(df_merge_by_zip_and_city.lat))    #369 rows attempted but still no success in joining
+
+
+#try one last time to see if the customer zips are found in the lookup table     
+df_merge_by_zip = df_merged2[ pd.isnull(df_merged2.lat) ][ cols ].merge(df_geo_avg_by_zip2, how='left', left_on=['customer_zip_code_prefix'], right_on=['zip_prefix'])
+df_merge_by_zip.drop(['zip_prefix'], axis='columns', inplace=True)
+df_merge_by_zip.info()    #369 rows attempted, 52 lat/longs found
+sum(pd.isnull(df_merge_by_zip.lat))    #317.    52 rows were found, 317 were not
+
+
+#copy the rows for which we did NOT find a lat long  in the last attempt 
+rw_ids_fnd_by_zip_only = df_merge_by_zip[pd.notnull(df_merge_by_zip.lat)].Rww_ID
+df_merged3 = df_merged2[ ~  df_merged2.Rww_ID.isin(rw_ids_fnd_by_zip_only)  ].copy()
+
+#len(  df_merged2[ ~  df_merged2.Rww_ID.isin(rw_ids_fnd_by_zip_only)  ]  )
+
+#add in the rows for which we found a lat long  in the last attempt 
+df_merged3 = df_merged3.append( df_merge_by_zip[ pd.notnull(df_merge_by_zip.lat) ].copy() )
+
+#rename the lat, long columns as lat_cust, long_cust
+new_col_names = [col for col in df_merged3.columns[:-2]]
+new_col_names.append('lat_customer')
+new_col_names.append('long_customer')
+df_merged3.columns = new_col_names
+
+df_merged3.info()
+
+
+
+
+
+
+
+# ==============================
+#repeat for the seller lat/longs
+# ==============================
+df_merged4 = df_merged3.merge(df_geo_avg_by_zip_city_state2, how='left', left_on=['seller_zip_code_prefix', 'seller_city', 'seller_state'], right_on=['zip_prefix', 'city', 'state'])
+#df_merged4.info()  #stiil have 118,318 as expected
+sum(pd.isnull(df_merged4.lat))  #3244     
+sum(pd.isna(df_merged3.seller_zip_code_prefix))  #0     
+sum(pd.isnull(df_merged3.seller_city))  #0     
+sum(pd.isnull(df_merged3.seller_state))  #0 
+    #apparently some of the sellers are from zip/city/states not in the lat/long lookup table.   retry by zip and state
+    
+#prune out these join columns that have been added in unnecessarily
+df_merged4.drop(['zip_prefix', 'city', 'state'], axis='columns', inplace=True)
+
+
+#try joining on just zip & city and see if that succeeds (maybe the states were null or conflicting??)
+   
+#for the 2nd attempt at joining on zip&city, prune out the lat & long columns as a) they are null anyway and b) they cause diff lat&long cols to get created like lat_y, long_y
+cols = [col for col in df_merged4.columns if col not in ['lat', 'long']]
+df_merge_by_seller_zip_and_city = df_merged4[ pd.isnull(df_merged4.lat) ][ cols ].merge(df_geo_avg_by_zip_city2, how='left', left_on=['seller_zip_code_prefix', 'seller_city'], right_on=['zip_prefix', 'city'])
+df_merge_by_seller_zip_and_city.drop(['zip_prefix', 'city'], axis='columns', inplace=True)
+df_merge_by_seller_zip_and_city.info()    #666 rows w/ lat/long now
+sum(pd.isnull(df_merge_by_seller_zip_and_city.lat))    #still 2578 rows w/ no success in joining
+
+
+
+#copy the rows for which we did NOT find a lat long  in the last attempt 
+rw_ids_fnd_by_seller_zip_and_state = df_merge_by_seller_zip_and_city[pd.notnull(df_merge_by_seller_zip_and_city.lat)].Rww_ID
+#copy everything else into a new dataset
+df_merged5 = df_merged4[ ~  df_merged4.Rww_ID.isin(rw_ids_fnd_by_seller_zip_and_state)  ].copy()
+
+
+#add in the rows for which we found a lat long  in the last attempt 
+df_merged5 = df_merged5.append( df_merge_by_seller_zip_and_city[ pd.notnull(df_merge_by_seller_zip_and_city.lat) ].copy() )
+     
+df_merged5.info()
+#118,318, with 115,740 not null for lat/long
+
+
+#try one last time to see if the customer zips are found in the lookup table     
+df_merge_by_seller_zip = df_merged5[ pd.isnull(df_merged5.lat) ][ cols ].merge(df_geo_avg_by_zip2, how='left', left_on=['seller_zip_code_prefix'], right_on=['zip_prefix'])
+df_merge_by_seller_zip.drop(['zip_prefix'], axis='columns', inplace=True)
+df_merge_by_seller_zip.info()    #2578 rows attempted, 2313 lat/longs found
+sum(pd.isnull(df_merge_by_seller_zip.lat))    #265 rows still don't have a lat/long
+
+
+
+
+
+#copy the rows for which we did NOT find a lat long  in the last attempt 
+rw_ids_fnd_by_seller_zip_only = df_merge_by_seller_zip[pd.notnull(df_merge_by_seller_zip.lat)].Rww_ID
+df_merged6 = df_merged5[ ~  df_merged5.Rww_ID.isin(rw_ids_fnd_by_seller_zip_only)  ].copy()
+
+#len(  df_merged5[ ~  df_merged5.Rww_ID.isin(rw_ids_fnd_by_seller_zip_only)  ]  )
+
+#add in the rows for which we found a lat long  in the last attempt 
+df_merged6 = df_merged6.append( df_merge_by_seller_zip[ pd.notnull(df_merge_by_seller_zip.lat) ].copy() )
+
+#rename the lat, long columns as lat_cust, long_cust
+new_col_names = [col for col in df_merged6.columns[:-2]]
+new_col_names.append('lat_seller')
+new_col_names.append('long_seller')
+df_merged6.columns = new_col_names
+
+df_merged6.info()
+
+
+
+df_merged6.to_csv(wkg_dir+'Merged_dataset_w_LatLong.csv')
